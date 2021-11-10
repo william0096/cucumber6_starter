1834c5d1830784d639f37e39d105c8284f369268
jorge-castillo <jorge.castillo@avaldigitallabs.com>
2021-11-10 07:35:39 -0500

    Change report titles and selectors enhancement

--- src/test/java/starter/search/ReactShopping.java
+++ src/test/java/starter/search/ReactShopping.java
@@ -5,13 +5,13 @@ import net.serenitybdd.screenplay.targets.Target;
 
 public class ReactShopping {
 
-    public static Target SELECTSIZE = Target.the("size selection").locatedBy("//span[(text()='L')]");
-    public static Target SELECTTSHIRT1 = Target.the("t-shit selection").locatedBy("//div[@data-sku='10686354557628304']//div[@class='shelf-item__buy-btn']");
-    public static Target SELECTTSHIRT2 = Target.the("t-shit selection").locatedBy("//div[@data-sku='11033926921508488']//div[@class='shelf-item__buy-btn']");
-    public static Target SELECTTSHIRT3 = Target.the("t-shit selection").locatedBy("//div[@data-sku='39876704341265610']//div[@class='shelf-item__buy-btn']");
-    public static Target SELECTQUANTITY1 = Target.the("t-shit selection").locatedBy("//*[@id=\"root\"]/div/div[2]/div[2]/div[1]/div[4]/div/button[2]");
-    public static Target SELECTQUANTITY2 = Target.the("t-shit selection").locatedBy("//*[@id=\"root\"]/div/div[2]/div[2]/div[2]/div[4]/div/button[2]");
-    public static Target SELECTQUANTITY3 = Target.the("t-shit selection").locatedBy("//*[@id=\"root\"]/div/div[2]/div[2]/div[3]/div[4]/div/button[2]");
-    public static Target CHECKOUT = Target.the("t-shit selection").locatedBy("//div[contains(text(),'Checkout')]");
+    public static Target SELECTSIZE = Target.the("size filter").locatedBy("//span[(text()='L')]");
+    public static Target SELECTTSHIRT1 = Target.the("first t-shirt selection").locatedBy("//div[@class='shelf-item'][1]");
+    public static Target SELECTTSHIRT2 = Target.the("second t-shirt selection").locatedBy("//div[@class='shelf-item'][2]");
+    public static Target SELECTTSHIRT3 = Target.the("third t-shirt selection").locatedBy("//div[@class='shelf-item'][3]");
+    public static Target SELECTQUANTITY1 = Target.the("first t-shirt quantity selection").locatedBy("//*[@id=\"root\"]/div/div[2]/div[2]/div[1]/div[4]/div/button[2]");
+    public static Target SELECTQUANTITY2 = Target.the("second t-shirt quantity selection").locatedBy("//*[@id=\"root\"]/div/div[2]/div[2]/div[2]/div[4]/div/button[2]");
+    public static Target SELECTQUANTITY3 = Target.the("third t-shirt quantity selection").locatedBy("//*[@id=\"root\"]/div/div[2]/div[2]/div[3]/div[4]/div/button[2]");
+    public static Target CHECKOUT = Target.the("t-shirt checkout").locatedBy("//div[contains(text(),'Checkout')]");
 
 }
